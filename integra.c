8b481981b6d1fe11181dc2cd63c0a135e93e00e5
Amar Kumar <amaryadav4uall@gmail.com>
2018-04-01 00:58:14 +0530

    Update integra.c

--- integra.c
+++ integra.c
@@ -1,3 +1,8 @@
+Write a program to find the nearest smaller number array for the given array.Nearest smaller number is the number which is smaller than
+the given number and the nearest to the given number position. If no such number is found print -1. 
+for example: the given array is [5,2,4,6,1,2,5] then the nearest smaller number array is [2,1,2,1,0,1,2] like nearest smallest 5 is 2 and
+nearest smallest of 2 is 1 and 4 is (bcz 2 & 6 first occurance from left and right so we will take 2) and soon on....
+
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
